13dda7f0f0c32842890913bd96cbcf1aabd0b69b
Néstor Nicolás Campos Rojas <nescampos_18@hotmail.com>
2024-04-18 11:49:15 -0400

    Etherlink testnet added to the SDK

--- monetizado.js
+++ monetizado.js
@@ -1,12 +1,14 @@
 const networks = {
-    "opbnb:testnet":"0x0702B45d590372b5456BeC39e13a46c00Fc8F733",
-    "bnb:testnet":"0x13cbEaAaBcC8a126dF2f7b1aA955251574191231",
-	"base:testnet":"0x4dA05ddA2F0586327E956548fd3E88a508ba2168",
-	"botanix:testnet" : "0xd0876600e82CCAa4aA0ab0Cd8bEa9c74F5b46De3",
+    
 	"arbitrum:sepolia" : "0x3258814758AC48fE9c0869d43Df8F13aD0cB2A25",
+	"base:testnet":"0x4dA05ddA2F0586327E956548fd3E88a508ba2168",
 	"berachain:testnet" : "0x4FCA17c99Cd6eA508AD64D75Daa6a5BeBfdd5916",
+    "bnb:testnet":"0x13cbEaAaBcC8a126dF2f7b1aA955251574191231",
+	"botanix:testnet" : "0xd0876600e82CCAa4aA0ab0Cd8bEa9c74F5b46De3",
+	"etherlink:testnet" : "0x4dA05ddA2F0586327E956548fd3E88a508ba2168",
 	"filecoin:testnet" : "0x6388418CfEa921C323b91B173E10D7f8f1193680",
-	"mode:testnet" : "0xf0F90a236c860FB79DbBA4Ad9Dd62b714268BD21"
+	"mode:testnet" : "0xf0F90a236c860FB79DbBA4Ad9Dd62b714268BD21",
+	"opbnb:testnet":"0x0702B45d590372b5456BeC39e13a46c00Fc8F733"
 }
 
 const networksEIP1559 = ["bnb","opbnb","base"];
